c5c3b2d009eed6424a67ff5c5a9c002027b971ae
Max Ageev <54005825+TobbyMax@users.noreply.github.com>
2024-03-30 20:56:09 +0300

    fix: add schema to postgres  (#6)

--- internal/adapters/postgres/migrations/tables.up.sql
+++ internal/adapters/postgres/migrations/tables.up.sql
@@ -1,3 +1,5 @@
+CREATE SCHEMA profiles;
+
 CREATE TYPE HABIT AS ENUM ('no', 'sometimes', 'yes', 'prefer not to say');
 CREATE TYPE INTENTION AS ENUM (
     'life partner',
@@ -11,7 +13,7 @@ CREATE TYPE GENDER AS ENUM ('man', 'woman');
 CREATE TYPE PREFERRED_PARTNER AS ENUM ('men', 'women', 'anyone');
 CREATE TYPE FAMILY_PLANS AS ENUM ('don''t want children', 'want children', 'open to children', 'not sure yet', 'prefer not to say');
 
-CREATE TABLE profiles
+CREATE TABLE profiles.profiles
 (
     user_id           uuid,
     first_name        TEXT,
@@ -29,7 +31,7 @@ CREATE TABLE profiles
     PRIMARY KEY (user_id)
 );
 
-CREATE TABLE prompts
+CREATE TABLE profiles.prompts
 (
     id uuid,
     user_id uuid,
