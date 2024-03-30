7188adadbaac2b2e3ed5836fe9579ca12c490261
Max Ageev <54005825+TobbyMax@users.noreply.github.com>
2024-03-31 01:42:17 +0300

    feat: TIN-85 add recomedation handle (#8)

--- internal/adapters/postgres/queries.go
+++ internal/adapters/postgres/queries.go
@@ -12,8 +12,9 @@ const (
 							    sex = $5, preferred_partner = $6, intention = $7, height = $8,
 							    has_children = $9, family_plans = $10, location = $11,
 							    drinks_alcohol = $12, smokes = $13 WHERE user_id = $1 RETURNING *`
-	getMultipleProfilesByIDsQuery = `SELECT * FROM profiles.profiles WHERE user_id = ANY($1)`
-	createPromptQuery             = `INSERT INTO profiles.prompts (uid, user_id, question, answer, position) VALUES ($1, $2, $3, $4, $5)`
-	getPromptsQuery               = `SELECT * FROM profiles.prompts WHERE user_id = $1 ORDER BY position ASC`
-	updatePromptQuery             = `UPDATE profiles.prompts SET question = $2, answer = $3, position = $4 WHERE uid = $1 RETURNING *`
+	getRandomProfileBySexAndPreferenceQuery = `SELECT * FROM profiles.profiles WHERE (user_id != $1 AND (sex = $2 OR sex = $3) AND (preferred_partner = $4 OR preferred_partner = 'anyone')) ORDER BY RANDOM() LIMIT 1`
+	getMultipleProfilesByIDsQuery           = `SELECT * FROM profiles.profiles WHERE user_id = ANY($1)`
+	createPromptQuery                       = `INSERT INTO profiles.prompts (uid, user_id, question, answer, position) VALUES ($1, $2, $3, $4, $5)`
+	getPromptsQuery                         = `SELECT * FROM profiles.prompts WHERE user_id = $1 ORDER BY position ASC`
+	updatePromptQuery                       = `UPDATE profiles.prompts SET question = $2, answer = $3, position = $4 WHERE uid = $1 RETURNING *`
 )
---
+++ internal/models/preference.go
@@ -0,0 +1,16 @@
+package models
+
+const (
+	Anyone Preference = "anyone"
+	Man    Preference = "man"
+	Woman  Preference = "woman"
+)
+
+type Preference string
+
+func (s Preference) Preferences() (string, string) {
+	if s == Anyone {
+		return string(Man), string(Woman)
+	}
+	return string(s), string(s)
+}
