9835bbf243af08bd25ae059bba117fa2f7f94f3a
TobbyMax <ageev.maxim2003@gmail.com>
2024-04-08 22:52:25 +0300

    fix: prompt queries

--- internal/adapters/postgres/queries.go
+++ internal/adapters/postgres/queries.go
@@ -14,7 +14,7 @@ const (
 							    drinks_alcohol = $12, smokes = $13 WHERE user_id = $1 RETURNING *`
 	getRandomProfileBySexAndPreferenceQuery = `SELECT * FROM profiles.profiles WHERE (user_id != $1 AND (sex = $2 OR sex = $3) AND (preferred_partner = $4 OR preferred_partner = 'anyone')) ORDER BY RANDOM() LIMIT 1`
 	getMultipleProfilesByIDsQuery           = `SELECT * FROM profiles.profiles WHERE user_id = ANY($1)`
-	createPromptQuery                       = `INSERT INTO profiles.prompts (uid, user_id, question, answer, position) VALUES ($1, $2, $3, $4, $5)`
+	createPromptQuery                       = `INSERT INTO profiles.prompts (id, user_id, question, answer, position) VALUES ($1, $2, $3, $4, $5)`
 	getPromptsQuery                         = `SELECT * FROM profiles.prompts WHERE user_id = $1 ORDER BY position ASC`
-	updatePromptQuery                       = `UPDATE profiles.prompts SET question = $2, answer = $3, position = $4 WHERE uid = $1 RETURNING *`
+	updatePromptQuery                       = `UPDATE profiles.prompts SET question = $2, answer = $3, position = $4 WHERE id = $1 RETURNING *`
 )
