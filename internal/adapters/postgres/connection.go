6c8aecc7fe089e9e1cd175caecb39a6c6af467b9
TobbyMax <ageev.maxim2003@gmail.com>
2024-05-12 16:13:52 +0300

    chore: TIN-104 refactor pg connection

--- internal/adapters/postgres/connection.go
+++ internal/adapters/postgres/connection.go
@@ -2,25 +2,42 @@ package postgres
 
 import (
 	"context"
-	"github.com/jackc/pgx/v5/pgxpool"
+	"fmt"
 	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func Connect() (*pgxpool.Pool, error) {
-	connString := "host=postgres port=5432 dbname=glimpse user=glimpse password=password sslmode=disable"
+type Config struct {
+	Host              string
+	Port              int
+	User              string
+	Password          string
+	DBName            string
+	SSLMode           string
+	ConnectionTimeout time.Duration
+}
+
+func Connect(ctx context.Context, cfg Config) (*pgxpool.Pool, error) {
+	connString := getConnectionString(cfg)
 	poolCfg, err := pgxpool.ParseConfig(connString)
 	if err != nil {
 		return nil, err
 	}
-	pool, err := pgxpool.NewWithConfig(context.Background(), poolCfg)
+	pool, err := pgxpool.NewWithConfig(ctx, poolCfg)
 	if err != nil {
 		return nil, err
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	ctx, cancel := context.WithTimeout(ctx, 60*time.Second)
 	defer cancel()
 	err = pool.Ping(ctx)
 	if err != nil {
 		return nil, err
 	}
 	return pool, err
+}
+
+func getConnectionString(cfg Config) string {
+	return fmt.Sprintf("host=%s port=%d dbname=%s user=%s password=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.DBName, cfg.User, cfg.Password, cfg.SSLMode)
 }
