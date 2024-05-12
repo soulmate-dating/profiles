6fb234ba040f7fca398833aaeedd52ca1217ff36
TobbyMax <ageev.maxim2003@gmail.com>
2024-05-12 18:01:22 +0300

    chore: set required env vars

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,22 +8,22 @@ import (
 )
 
 type Postgres struct {
-	Host              string        `env:"POSTGRES_HOST" example:"localhost"`
-	Port              int           `env:"POSTGRES_PORT" envDefault:"5432"`
-	User              string        `env:"POSTGRES_USER" example:"glimpse"`
-	Password          string        `env:"POSTGRES_PASSWORD" example:"password"`
-	Database          string        `env:"POSTGRES_DB" example:"glimpse"`
+	Host              string        `env:"POSTGRES_HOST,required" example:"localhost"`
+	Port              int           `env:"POSTGRES_PORT,required" envDefault:"5432"`
+	User              string        `env:"POSTGRES_USER,required" example:"glimpse"`
+	Password          string        `env:"POSTGRES_PASSWORD,required" example:"password"`
+	Database          string        `env:"POSTGRES_DB,required" example:"glimpse"`
 	SSLMode           string        `env:"POSTGRES_SSL_MODE" envDefault:"disable"`
 	ConnectionTimeout time.Duration `env:"POSTGRES_CONNECTION_TIMEOUT" envDefault:"60s"`
 }
 
 type API struct {
 	Network string `env:"API_NETWORK" envDefault:"tcp"`
-	Address string `env:"API_ADDRESS" envDefault:"localhost:8080"`
+	Address string `env:"API_ADDRESS,required" example:"localhost:8080"`
 }
 
 type Media struct {
-	Address   string `env:"MEDIA_ADDRESS" envDefault:"localhost:8081"`
+	Address   string `env:"MEDIA_ADDRESS,required" example:"localhost:8081"`
 	EnableTLS bool   `env:"MEDIA_ENABLE_TLS" envDefault:"false"`
 }
 
